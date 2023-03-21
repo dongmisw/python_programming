552994fba600d50b0115157eff5a6f711c716b8e
hong <hong@dm.ac.kr>
2023-03-21 13:07:53 +0900

    chap2

---
+++ chap2/1.py
@@ -0,0 +1,38 @@
+# 1.py
+# 동양미래대
+# 파이썬 프로그래밍
+''' 이것은 
+여러줄로 이루어진 
+주석입니다.
+'''
+
+print("hello 'world'!!" * 3 )
+print(3)
+print(type(3))
+print(1.1)
+print(type(1.1))
+
+print('3+15/2')
+print(eval('3+15/2'))
+
+print("hd")
+#print('hello "world"!!') 
+#print(type("hello"))
+
+print(1+62-3*52)
+print(eval('1+62-3*52'))
+
+print(eval('33333%222')+eval('33333//222'))
+
+a = 78000 // 50000
+print("5만원권 " , a , "장 냅니다.")
+result = 78000%50000
+print(result , "원 남았습니다. ")
+b= result //10000
+print("만원짜리 지폐 " , b , "장 냅니다." )
+
+
+
+
+
+
