1593a864ba226d71d7898700b0b6f884a8a8dd6e
hellen1221 <hsrhee00@gmail.com>
2023-03-29 16:03:04 +0900

    a

--- chap3/0329.py
+++ chap3/0329.py
@@ -43,3 +43,41 @@ print(type(age))
 #int("30")
 #float("20.6")
 #str(30)
+
+#a= "python"
+'''
+a ="동양미래대학교"
+print(a[0], "  ",  a[1], "..." , a[5]) #p y n
+print("python"[0], "... ", "python"[1], " ..." , "python"[5])
+print("동양미래대학교"[8])
+print(len(a))
+'''
+'''
+school = "동양미래대학교-컴퓨터소프트웨어공학과"
+print("school[::-1] : "   , school[::-1])
+print("school[::] : "   , school[::]) #동양미래대학교-컴퓨터소프트웨어공학과
+print("school[0:len(school):2] : " ,school[0:len(school):2]) #동미대교컴....
+print("school[8:len(school):2] : ", school[8:len(school):2])#컴터프웨공과
+print("school[8:len(school)] : ", school[8:len(school)]) #컴퓨터소프트웨어공학과
+print("school[:15:4]  : ", school[:15:4])
+#동양미래대학교-컴퓨터소프트웨어  4칸씩 뛰기.동대컴프
+
+'''
+
+'''
+school="동양미래대학교"
+print("school[:] : "   , school[:]) #동양미래대학교
+print("school[0:3] : " ,school[0:3]) #동양미
+print("school[1:4] : ", school[1:4])#양미래
+print("school[2:4] : ", school[2:4]) #미래
+print("school[1:]  : ", school[1:])#양미래대
+ 
+'''
+
+
+
+
+
+
+
+
