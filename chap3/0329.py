a699695c4f84a59ba50c2f5c78c78a52192dbbe0
hellen1221 <hsrhee00@gmail.com>
2023-03-29 15:07:12 +0900

    1

--- chap3/0329.py
+++ chap3/0329.py
@@ -1,1 +1,45 @@
-#주석 
+#0329
+'''
+print("안녕하세요.")
+print(3)
+print(10.6)
+
+var = "안녕"
+print(var)
+print(type(var))
+print(type(3))
+
+var1 = 3
+_var = 10
+height = 180
+weight = 50
+length_list = 
+'''
+'''
+
+a,b = 100,10
+a *= b   
+print("a*=b : ",  a)
+
+a,b = 100,10
+a /= b   
+print("a/=b : ",  a)
+
+a,b = 100,10
+a -= b   
+print("a-=b : ",  a)
+'''
+'''
+
+name = input("이름이 뭔가요?")
+print("제 이름은", name, "입니다.")
+age = input("나이는?")
+print("나이는", age, "입니다.")  # "20"   , int("20")
+print("아버지 나이는 ", int(age) +30, "입니다.")
+print(type(age))
+'''
+
+
+#int("30")
+#float("20.6")
+#str(30)
