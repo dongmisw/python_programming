33cb5c9d053b45d2f9d3545dde50ca11af28ca6f
hellen1221 <hsrhee00@gmail.com>
2023-04-05 16:48:14 +0900

    040555

--- chap4/0405.py
+++ chap4/0405.py
@@ -106,7 +106,7 @@ for i in range(3,21,3):
 
 for i in range(20):
     print("i : " , i)
-'''
+
 
 #1부터 10까지 합을 구하시오.
 #2가지 방법으로, range도 쓰고, 그냥 명시도 하고
@@ -124,17 +124,94 @@ for i in range(1,11,1) :
     sum += i
     #sum = sum + i
     print( i ," 번째 loop입니다. sum은 ", sum ," 입니다.")
+else:
+    print("else안의 문구: for문이 잘 종료됨.")
+
+print(" 완전 바깥. else밖의 문구 : for 문 종료됨")
+   
  
-#else할것.
+ 
+# print(i , end=' ' )
+# while
+while 조건 :
+    수행문1
+else:
+    수행문2   
+
+#sum , 0-10까지 숫자를 찍음
+# sum을 구할것임.
+sum = 0
+n=0
+while n<11 :   
+    sum += n
+    print(n, "  번째 sum  : ", sum  )
+    n= n+1
+
+print("while 밖에서 sum의 값  : ", sum)
+
+while False:
+    print("실행이 되지 않음")
+
+while 0:
+    print("실행이 되지 않음")
+
+print("while 0 다음 줄입니다. ")
+
+#while True :
+#    print("무한루프")
+
+#while False:
+#    print("실행이 되지 않음")
+    
+'''
+
+'''
+#break continue 
+# 단어 입력을 무한 루프로 받는다.
+# 그 글자를 3번 써줌
+#"exit" -> 루프를 끝내고 종료
+#"apple" -> 3번 안쓰고 그냥 다시 단어 입력을 받음.
+
+while True :
+    word = input("단어를 입력하세요.")
+    if word =="exit":
+        print("넌 exit 을 입력했다. break를 만난다.")
+        break
+        print("break 뒤의 문장 ")
+    elif word =="apple" :
+        print("넌 apple을 입력했다. continue를 만난다.")
+        continue
+        print("continue뒤의 문장")
+    else:
+        for i in range(3):
+            print(word, end=' ')
+        print("해당 단어 끝")
+
+    print("apple을 넣으면 이걸 절대 볼수 없음")
 
 
+'''
+'''
+while True :
+    단어 입력을 받는다. 
+    if exit인경우:
+        break
+    elif apple인 경우:   
+        continue
+    else :
+        for :   
+            3번 찍는다.
+'''
 
 
-
-
-
-
-
+#놀이동산 놀이기구 탑승 문제
+#총 정원 4명 
+#정원이 차면, 놀이기구 시작
+#조건 키 150 이상만 탈수 있음.
+#사람들한테 키를 물어보고, 탑승시키시오. 
+#150이상 4명이 되면 놀이기구를 시작
+#수도코드를 작성해볼것. 
+#진짜코드도 작성할것
 
 
 
