5760386c7d9810ae1548be2912967d61765ff5a5
hellen1221 <hsrhee00@gmail.com>
2023-05-09 12:01:11 +0900

    0509

---
+++ chap8/hong/ai/hiai.py
@@ -0,0 +1,15 @@
+#hello.py
+
+def helloworld():
+    print("hello world!!")
+
+
+if __name__ == "__main__":
+    print("hello.py  __name__ : " ,__name__)  
+    print("오늘은 module 처음 배운 날입니다.")
+    print("오늘은 날씨는 24도 입니다.")
+    helloworld()
+else:
+    print("hello.py __name__ : " ,__name__)  
+
+
